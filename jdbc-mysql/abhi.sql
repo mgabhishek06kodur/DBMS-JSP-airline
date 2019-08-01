68db6ee20953c1b827636eff0a719c2709ec93bc
mgabhishek06kodur <mgabhishek06kodur@gmail.com>
2019-08-01 23:41:30 +0530

    first commit

---
+++ jdbc-mysql/abhi.sql
@@ -0,0 +1,75 @@
+insert into cancellation values(1111,11,'2017-1-2',10000);
+insert into cancellation values(1112,12,'2017-2-1',9000);
+insert into cancellation values(1113,13,'2017-3-4',8000);
+insert into cancellation values(1114,14,'2015-4-5',10000);
+insert into cancellation values(1115,15,'2016-6-8',70000);
+insert into cancellation values(1116,16,'2015-7-9',10000);
+insert into cancellation values(1117,17,'2016-11-12',11000);
+
+
+insert into classes values(111,100,'abz','a1',70000,15);
+insert into classes values(112,200,'abx','a2',60000,20);
+insert into classes values(113,300,'abs','a3',65000,15);
+insert into classes values(114,400,'abt','a4',60000,30);
+insert into classes values(115,500,'cmr','a5',50000,30);
+insert into classes values(116,600,'sst','a6',40000,15);
+insert into clsses values(117,700,'jar','a7',10000,30);
+
+
+insert into flight_days values(333,100,'2017-01-1','9-30');
+insert into flight_days values (334,200,'2017-01-16','8-30');
+insert into flight_days values (335,300,'2017-03-15','7-30');
+insert into flight_days values(336,400,'2015-3-21','9-00');
+insert into flight_days values(337,500,'2016-6-15','11-30');
+insert into flight_days values(338,600,'2015-7-28','12-00');
+insert into flight_days values(339,700,'2016-11-18','1-30');
+insert into flight_days values(330,800,'2017-4-14','5-00');
+
+
+insert passanger values(12345,100,'1986-01-10','kundapura','india''harish','male',9213456870,'hari54@gmail.com',5601,11);
+insert passanger values(12346,200,'1976-02-09','udupi','hindu''girish','male',9213456760,'giri54@gmail.com',5602,12);
+insert passanger values(12347,300,'1996-04-15','tumkur','hindu''suhas','male',9213456860,'suhasa84@gmail.com',5603,13);
+insert passanger values(12348,400,'1988-07-11','raichr','hindu''sudhir','male',9213456870,'sudhi64@gmail.com',5605,14);
+insert passanger values(12349,500,'1984-08-19','manipal','muslim''ridan','male',9213456871,'ridu14@gmail.com',5606,15);
+insert passanger values(12350,600,'2000-08-21','bijapur','hindu''manikanta','male',9288856870,'mani54@gmail.com',5607,16);
+insert passanger values(12351,700,'1999-01-10','banglore','india''manish','male',9213456877,'mani54@gmail.com',5608,17);
+insert passanger values(12352,800,'1996-08-01','manglore','hindu''suma','female',9213456999,'suma444@gmail.com',5609,11);
+insert passanger values(12353,900,'1995-09-07','kundapura','hindu''gowri','female',9213456567,'gowri101@gmail.com',5610,12);
+insert passanger values(12354,1000,'1996-01-02','shimogga','hindu''abhishek','male',9213456111,'abhi470@gmail.com',5611,13);
+insert passanger values(12355,2000,'1986-01-10','kundapura','hindu''raj','male',9213423270,'raj123@gmail.com',5612,14);
+insert passanger values(12356,300,'1991-02-22','kundapura','hindu''danush','male',9098456870,'dhan255@gmail.com',5613,15);
+
+
+insert into payment values(701,1231,7843,70000,'2016-12-20',12345);
+insert into payment values(702,1232,7844,60000,'2017-01-04',12346);
+insert into payment values(703,1231,7843,65000,'2017-03-07',12347);
+insert into payment values(704,1231,7843,40000,'2015-03-01',12349);
+insert into payment values(705,1231,7843,70000,'2016-06-02',12350);
+insert into payment values(706,1231,7843,30000,'2015-07-09',12351);
+insert into payment values(707,1231,7843,10000,'2016-11-03',12352);
+
+
+insert Reservation values(11,100,'2017-01-01','delhi','chennai',70000,'9-30');
+insert Reservation values(12,200,'2017-01-16','banglore','mumbai',60000,'8-30');
+insert Reservation values(13,300,'2017-03-15','cochin','kolkatta',65000,'7-30');
+insert Reservation values(14,400,'2015-03-21','vishakapattanam','mumbai',70000,'9-00');
+insert Reservation values(15,500,'2016-06-15','chennai','delhi',50000,'11-30');
+insert Reservation values(16,600,'2015-07-28','hyderbad','surat',40000,'12-00');
+insert Reservation values(17,700,'2016-11-18','delhi','andaman',10000,'1-30');
+insert Reservation values(11,800,'2017-04-14','goa','chennai',70000,'5-00');
+insert Reservation values(12,900,'2017-09-01','manglore','banglore',30000,'5-30');
+insert Reservation values(13,1000,'2017-04-04','mumbai','chendighar',10000,'11-30');
+insert Reservation values(14,2000,'2017-07-08','delhi','chennai',65000,'4-30');
+insert Reservation values(15,3000,'2017-08-11','chennai','ranchi',60000,'3-15');
+
+
+
+insert into reservation check(11,'chandan','12mangl');
+insert into reservation check(12,'latha','lt12nm');
+insert into reservation check(13,'rachitha','rach123');
+insert into reservation check(14,'rakshitha','56rak12');
+insert into reservation check(15,'sundar','jai90');
+insert into reservation check(16,'sathish','123hhh');
+insert into reservation check(17,'manu','mla23');
+
+
